a5e2e0b8b0a4d5232042ac9a787a4867a3dd6c70
saifutdinovalex <39852471+saifutdinovalex@users.noreply.github.com>
2020-06-14 10:12:50 +0300

    Update ManagerRelation.php

--- models/manager_notification/relation_ar/ManagerRelation.php
+++ models/manager_notification/relation_ar/ManagerRelation.php
@@ -5,25 +5,10 @@ use Yii;
 
 trait ManagerRelation
 {
-	  public function getUsersData()
+	public function getUsersData()
     {
-        return $this->hasOne(\backend\models\ar\ArUsers::class, ['user_id' => 'key_id']);
+        return $this->hasOne(\backend\models\Users::class, ['id' => 'key_id']);
     }
-
-    public function getQueryData(){
-        return $this->hasOne(\backend\models\ar\ArQueries::class, ['id' => 'key_id']);
-    }
-
-    public function getUserSmiData()
-    {
-        return $this->hasOne(\backend\models\ar\ArUserSmi::class, ['id' => 'key_id']);
-    }
-
-    public function getVpRequestData()
-    {
-        return $this->hasOne(\backend\models\ar\ArVpRequest::class, ['id' => 'key_id']);
-    }
-	
 }
 
 ?>
